893b319b8402b7035e391832026ccab88c4b1d38
vaibhav1470 <107416768+vaibhav1470@users.noreply.github.com>
2022-07-17 08:28:51 +0530

    Function assignment 17-07-2022

---
+++ MaxOfThree.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class MaxOfThree{
+
+	public static void main(String[] args){
+		System.out.print("Enter the Numbers: ");
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		max(a,b,c);
+		System.out.println();
+		min(a,b,c);
+		}
+
+	static void max(int x, int y, int z){
+		if(x>y){
+			if(x>z){
+				System.out.print("Max: "+x);
+			}else{
+				System.out.print("Max: "+z);
+			}
+		}else if(y>z){
+			System.out.print("Max: "+y);
+		}else{
+			System.out.print("Max: "+z);
+		}
+
+	}
+	static void min(int x, int y, int z){
+		if(x<y){
+			if(x<z){
+				System.out.print("Min: "+x);
+			}else{
+				System.out.print("Min: "+z);
+			}
+		}else if(y<z){
+			System.out.print("Min: "+y);
+		}else{
+			System.out.print("Min: "+z);
+		}
+
+	}
+}
